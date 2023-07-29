14b8c4614554ec8619598804dd546a17309f5ecf
dangchinh25 <dangchinh2501@gmail.com>
2023-07-29 13:20:49 -0500

    Remove prisma and use Typeorm instead

--- config/index.ts
+++ config/index.ts
@@ -9,5 +9,10 @@ dotenv.config( { path: '.env' } );
 
 export const env = cleanEnv( process.env, {
     DATABASE_URL: str( { default: undefined } ),
+    POSTGRES_USER: str(),
+    POSTGRES_PASSWORD: str(),
+    POSTGRES_HOST: str(),
+    POSTGRES_PORT: str(),
+    POSTGRES_DB: str(),
     OPEN_AI_KEY: str()
 } );
--- index.ts
+++ index.ts
@@ -1,18 +1,50 @@
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { HumanMessage } from 'langchain/schema';
 import { env } from './config';
+import { OpenAI } from 'langchain/llms/openai';
+import { SqlDatabase } from 'langchain/sql_db';
+import { createSqlAgent, SqlToolkit } from 'langchain/agents/toolkits/sql';
+import { DataSource } from 'typeorm';
 
 const main = async () => {
-    const chat = new ChatOpenAI( {
-        temperature: 0,
-        openAIApiKey: env.OPEN_AI_KEY
+    const appDataSource = new DataSource( {
+        type: 'postgres',
+        host: env.POSTGRES_HOST,
+        port: Number( env.POSTGRES_PORT ),
+        username: env.POSTGRES_USER,
+        password: env.POSTGRES_PASSWORD,
+        database: env.POSTGRES_DB,
+        synchronize: true,
+        entities: [],
+        migrations: [],
+        subscribers: []
     } );
 
-    const result = await chat.predictMessages(
-        [ new HumanMessage( 'Translate this sentence from English to Vietnamese. I love programming.' ) ]
+    const db = await SqlDatabase.fromDataSourceParams(
+        { appDataSource: appDataSource }
     );
 
-    console.log( result );
+    const model = new OpenAI( {
+        openAIApiKey: env.OPEN_AI_KEY,
+        temperature: 0
+    } );
+
+    const toolkit = new SqlToolkit( db, model );
+    const executor = createSqlAgent( model, toolkit );
+
+    const input = 'Describe the playlisttrack table';
+
+    console.log( `Executing with input "${ input }"...` );
+
+    const result = await executor.call( { input } );
+
+    console.log( `Got output ${ result.output }` );
+
+    console.log(
+        `Got intermediate steps ${ JSON.stringify(
+            result.intermediateSteps,
+            null,
+            2
+        ) }`
+    );
 };
 
 main().then();
